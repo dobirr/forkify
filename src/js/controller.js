03d35b786fec1c9d6f70a28e10ca4a29419f51ca
Dennis Dobirr <me@dennisdobirr.de>
2023-02-26 14:43:10 +0100

    added model and controller for search

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,10 @@
 import * as model from './model';
 import recipeView from './views/recipeView';
+import searchView from './views/searchView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { state } from './model';
 
 const controllerRecipes = async function() {
 
@@ -24,9 +26,25 @@ const controllerRecipes = async function() {
     }
 }
 
+const controlSearchResults = async function() {
+    try {
+        // 1) Get search query
+        const query = searchView.getQuery();
+        if(!query) return;
+
+        // 1) Load search result
+        await model.loadSearchResults(query);
+
+        // 3) Render result
+        console.log(model.state.search);
+    } catch(err) {
+
+    }
+}
 
 const init = function() {
-    recipeView.addHandlerRender(controllerRecipes)
+    recipeView.addHandlerRender(controllerRecipes);
+    searchView.addHandlerSearch(controlSearchResults);
 }
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -2,12 +2,16 @@ import {API_URL} from './config';
 import {getJSON} from './helpers';
 
 export const state = {
-    recipe: {}
+    recipe: {},
+    search: {
+        query: '',
+        results: []
+    }
 }
 
 export const loadRecipe = async function(pId) {
     try {
-        const data = await getJSON(`${API_URL}/${pId}`);
+        const data = await getJSON(`${API_URL}${pId}`);
         let {id, title, publisher, cooking_time: cookingTime, image_url: imageUrl, ingredients, servings, source_url: sourceUrl} = data.data.data.recipe;
         state.recipe = {
             id,
@@ -19,6 +23,29 @@ export const loadRecipe = async function(pId) {
             servings,
             sourceUrl
         }
+    } catch(err) {
+        console.error({
+            error_model: err.message
+        });
+        throw err;
+    }
+}
+
+export const loadSearchResults = async function(query) {
+    try {
+        state.search.query = query;
+
+        const data = await getJSON(`${API_URL}?search=${query}`);
+        state.search.results = data.data.data.recipes.map(recipe => {
+            const {id, title, publisher, image_url: imageUrl} = recipe;
+            return {
+                id,
+                title,
+                publisher,
+                imageUrl
+            }
+        })
+
     } catch(err) {
         console.error({
             error_model: err.message
